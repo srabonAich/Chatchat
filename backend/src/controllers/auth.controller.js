96c4b135307ad28175df17399c58b556c7b79140
srabonAich <srabon-2022115890@cs.du.ac.bd>
2025-11-07 09:55:19 +0600

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,6 +70,10 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
+    if (!email || !password) { 
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
     try {
         const user = await User.findOne({ email });
         if (!user) {
