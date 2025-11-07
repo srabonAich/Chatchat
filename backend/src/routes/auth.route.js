d1e33dfabc83030575d6449975ef55a5064c97a4
srabonAich <srabon-2022115890@cs.du.ac.bd>
2025-11-07 23:53:41 +0600

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,16 @@
-import express from 'express';
-import { signup, login, logout, updateProfile} from '../controllers/auth.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
-
+import express from "express";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { acrjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(acrjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
@@ -11,6 +18,8 @@ router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+router.get("/check", protectRoute, (req, res) =>
+  res.status(200).json(req.user)
+);
 
 export default router;
