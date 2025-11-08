c6b34315151e2e38b0256791ccaeb8aa66089570
srabonAich <srabon-2022115890@cs.du.ac.bd>
2025-11-08 14:05:48 +0600

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,19 @@
-import e from 'express';
-import express from 'express';
+import express from "express";
+import { getAllContacts } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessagesByUserId } from "../controllers/message.controller.js";
+import { sendMessage } from "../controllers/message.controller.js";
+import { getChatPartners } from "../controllers/message.controller.js";
+import { acrjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("Send message endpoint");
-});
+//this requests will get rate limited and then authenticated
+router.use(acrjetProtection,protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
